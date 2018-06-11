1ba7521131277991dcdf32ba8751b55a32e41834
Abel Alonso <abel.alonso@gmail.com>
2018-06-11 15:42:17 +0100

    Name and inputs and conditionals

--- starter-code/basic-algorithms.js
+++ starter-code/basic-algorithms.js
@@ -1,7 +1,20 @@
 // Names and Input
+var hacker1 = "Abel";
+var hacker2 = prompt("Who is hacker 2");
+
+console.log("The driver is "+hacker1);
+console.log ("The navigator is "+hacker2);
 
 
 //Conditionals
-
+if (hacker1.length>hacker2.length){
+    console.log ("The driver has the longest name, it has "+hacker1.length+" characters.");
+  } else if (hacker1.length<hacker2.length){
+    console.log ("The navigator has the longest name, it has "+hacker2.length+" characters");
+  } else {
+    console.log("Wow, you both got equally long names, "+hacker1.length+" characters");
+  }
+  
+  
 
 // Lorem ipsum generator
